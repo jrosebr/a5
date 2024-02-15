693a8c6449281b295834582a788ed64e38177408
jrosebr <jrosebr@iu.edu>
2024-02-15 00:29:31 -0500

    Update a5.rkt

--- a5.rkt
+++ a5.rkt
@@ -1,6 +1,9 @@
 #lang racket
 
 ;Problem 1
+
+(require racket/trace)
+
 (define value-of-ds
   (λ (e env)
     (match e
@@ -27,29 +30,47 @@
       
       (`empty '())
       
-      (`,y #:when (symbol? y) (env y))
+      (`,y #:when (symbol? y) (apply-env-ds env y))
       
       (`(let ((,x ,v)) ,body)
        #:when (symbol? x)
        (let ((val (value-of-ds v env)))
-         (value-of-ds body (λ (y)
-                             (cond
-                               ((eqv? y x) val)
-                               (else (env y)))))))
+         (value-of-ds body (extend-env-ds x val env))))
       
       (`(λ (,x) ,body)
        #:when (symbol? x)
        ;; Replacing the formal env with env^ turns this into
        ;; a lexically scoped interpreter
-       (λ (arg env)
-         (value-of-ds body (λ (y)
-                             (cond
-                               ((eqv? y x) arg)
-                               (else (env y)))))))
+       `(closure ,x ,body ,env))
       
       (`(,rator ,rand)
-       ((value-of-ds rator env) (value-of-ds rand env) env)))))
+       (let ((closure (value-of-ds rator env)))
+         (apply-clos-ds closure rand env))))))
+
+(trace value-of-ds)
 
 
+(define empty-env-ds
+  (λ ()
+    '()))
 
+(define extend-env-ds
+  (λ (x arg env)
+    (cons (cons x arg) env)))
 
+(define apply-env-ds
+  (λ (env y)
+    (let ((binding (assoc y env)))
+      (if binding
+          (cdr binding)
+          (error 'apply-env-ds "unbound variable: ~a" y)))))
+
+(define make-clos-ds
+  (λ (x body env)
+    `(closure ,x , body ,env)))
+
+(define apply-clos-ds
+  (λ (closure rand env)
+    (match closure
+      (`(closure ,x ,body ,closure-env)
+       (value-of-ds body (extend-env-ds x rand closure-env))))))
